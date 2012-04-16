9c8c2edd20f14a87e9e82bb05429d6019a63ff18
= <=>
2012-04-16 15:31:03 +0200

    Comment deleted

--- install/index.php
+++ install/index.php
@@ -3,7 +3,6 @@
 /*
  * This file is part of Fork CMS.
  *
- * aangepast door Frederick
  * For the full copyright and license information, please view the license
  * file that was distributed with this source code.
  */
