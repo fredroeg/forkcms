c36ca0b69f3a2bd41da22dcbd94a034e59d44f8b
Frederick Roegiers <frederick.roegiers@gmail.com>
2012-05-04 14:13:38 +0200

    optimalisation

--- backend/modules/sea/engine/base.php
+++ backend/modules/sea/engine/base.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
- * This file is part of Fork CMS.
- *
- * For the full copyright and license information, please view the license
- * file that was distributed with this source code.
- */
-
 /**
  * This class implements a lot of functionality that can be extended by a specific action
  *
- * @author Annelies Van Extergem <annelies.vanextergem@netlash.com>
  * @author Frederick Roegiers <frederick.roegiers@wijs.be>
  */
 class BackendSeaBase extends BackendBaseActionIndex
