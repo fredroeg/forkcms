3e358f5b94fdfaa3eff4dda846a7a9824c915055
Frederick Roegiers <frederick.roegiers@gmail.com>
2012-05-22 16:48:41 +0200

    cronjob edited

---
+++ backend/modules/analytics/cronjobs/get_data.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This cronjob will fetch the requested data
+ *
+ * @author Frederick Roegiers <frederick.roegiers@wijs.be>
+ */
+class BackendAnalyticsCronjobGetData extends BackendBaseCronjob
+{
+	/**
+	 * Execute the action
+	 */
+	public function execute()
+	{
+		parent::execute();
+
+		// get parameters
+		$startTimestamp = trim(SpoonFilter::getGetValue('start_date', null, ''));
+		$endTimestamp = trim(SpoonFilter::getGetValue('end_date', null, ''));
+
+		$period = BackendAnalyticsModel::getLatestPeriod();
+		$periodId = $period['period_id'] + 1;
+		BackendAnalyticsHelper::insertAnalyticsData($startTimestamp, $endTimestamp, $periodId);
+	}
+}
